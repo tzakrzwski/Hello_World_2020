120ce9032e71ed769538c366fa9925ef0fc67b85
tzakrzwski <35140276+tzakrzwski@users.noreply.github.com>
2020-10-17 11:47:56 -0400

    a

--- src/discord_interface.py
+++ src/discord_interface.py
@@ -8,13 +8,10 @@ from dotenv import load_dotenv
 #Set-up
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
-
 client = discord.Client()
-
-#@client.event
-#async def on_ready():
-    #print(f'{client.user} has connected to Discord!')
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
 
 @client.event
 async def on_message(message):
