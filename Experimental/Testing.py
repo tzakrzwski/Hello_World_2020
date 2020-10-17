ac46baf2ea38fa17d9a279d30efbc389471aa0d8
tzakrzwski <35140276+tzakrzwski@users.noreply.github.com>
2020-10-17 09:49:28 -0400

    First working bot

--- Experimental/Testing.py
+++ Experimental/Testing.py
@@ -1,3 +1,17 @@
+#Just some test code for connecting to Discord bot
 import random
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
 
-print('Hello world')
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
