c1c23762b861c06aedd9a2b8b1170f7b9b12109d
cjanse <carterjanse@gmail.com>
2020-10-17 18:19:26 +0200

    Test to see if discord_interface.py can access game.py

--- src/game.py
+++ src/game.py
@@ -1,3 +1,10 @@
 #class for game logic
+import random
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
 import tribute.py
 class Game:
+	def runSimulation():
+		print('Hello')
