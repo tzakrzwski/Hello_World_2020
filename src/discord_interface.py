06b80022e6d95e8250be0acda7424e555435bc48
tzakrzwski <35140276+tzakrzwski@users.noreply.github.com>
2020-10-17 14:12:33 -0400

    Update discord_interface.py

--- src/discord_interface.py
+++ src/discord_interface.py
@@ -4,15 +4,38 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
+#Variables
+is_game = False #Tells if there is currently a game going on
 
 #Set-up
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-client = discord.Client()
+client = commands.Bot(command_prefix='!')
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+
+
+#Start-Game function
+#Asks sever members to dm the bot if they want to join
+#Has min, max number of players
+@client.command(name='begin')
+@commands.has_role('Admin')
+async def begin(ctx, min=8, max=20):
+    ctx.guild.channels[0].send('Yes?')
+    print('hello')
+    #if is_game and not ctx.guild: #Check if already in a game AND in a server
+       # return
+    
+    for member in ctx.guild.members:
+        print(member.nick)
+
+        
+
+
+'''
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -21,5 +44,5 @@ async def on_message(message):
     print(message.channel.__dir__())
     if message.content == 'Beeg':
         await message.channel.send('Do you guys have the Big Yoshi?')
-
+'''
 client.run(TOKEN)
