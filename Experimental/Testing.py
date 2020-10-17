d20881496cf5d91dd5569f1b2a0ef911b4e5f939
tzakrzwski <35140276+tzakrzwski@users.noreply.github.com>
2020-10-17 10:02:21 -0400

    BEEG

--- Experimental/Testing.py
+++ Experimental/Testing.py
@@ -7,11 +7,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
     print(f'{client.user} has connected to Discord!')
+    #f'{client.user} is connected to the following guild:\n'
+    #print(f'{guild.name}(id: {guild.id})')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content == 'Beeg':
+        await message.channel.send('Do you guys have the Big Yoshi?')
 
 client.run(TOKEN)
