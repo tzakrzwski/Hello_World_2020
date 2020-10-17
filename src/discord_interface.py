61d80bfee8d19cd6b5e600f1564f6a941d83a99c
tzakrzwski <35140276+tzakrzwski@users.noreply.github.com>
2020-10-17 11:41:54 -0400

    Added tribute

---
+++ src/discord_interface.py
@@ -0,0 +1,28 @@
+import random
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+
+#Set-up
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+#@client.event
+#async def on_ready():
+    #print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    print(message.channel.__dir__())
+    if message.content == 'Beeg':
+        await message.channel.send('Do you guys have the Big Yoshi?')
+
+client.run(TOKEN)
